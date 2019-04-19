7fe11105924874558cfd68eac55228b72b3799ae
Guy Daher <guy.daher@algolia.com>
2019-04-19 17:43:09 +0200

    new vm connectors

--- development-pods-instantsearch/RefinementList.swift
+++ development-pods-instantsearch/RefinementList.swift
@@ -18,6 +18,7 @@ class RefinementListController: UIViewController, UITableViewDataSource, UITable
 
   var refinementListViewModel: RefinementListViewModel!
   var syncRefinementListViewModel: RefinementListViewModel!
+  var refinementFacetsViewModel: RefinementFacetsViewModel!
   var tableView = UITableView()
   var textFieldWidget: TextFieldWidget!
   var toggle = UISwitch()
@@ -28,6 +29,7 @@ class RefinementListController: UIViewController, UITableViewDataSource, UITable
   var index: Index!
   var filterState: FilterState!
   var query: Query!
+  var sortedFacetValues: [SelectableRefinement] = []
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -62,106 +64,105 @@ class RefinementListController: UIViewController, UITableViewDataSource, UITable
     textFieldWidget = TextFieldWidget(textField: textField)
     client = Client(appID: ALGOLIA_APP_ID, apiKey: ALGOLIA_API_KEY)
     index = client.index(withName: ALGOLIA_INDEX_NAME)
-    refinementListViewModel = RefinementListViewModel(attribute: Attribute("category"), filterState: filterState)
-    syncRefinementListViewModel = RefinementListViewModel(attribute: Attribute("category"), filterState: filterState)
-    // when "and" + "true", we get a bug in result list, page offset by 1?
-    // When we use "and" + "true", it's the only time we are actually doing a .search() and not .searchDisjunctiveFaceting()
-    //refinementListViewModel.settings.operator = .and(selection: .multiple)
-    refinementListViewModel.settings.operator = .or
-    refinementListViewModel.settings.sortBy = [.isRefined, .count(order: .descending), .alphabetical(order: .ascending)]
-
-    syncRefinementListViewModel.settings.operator = .or
-    syncRefinementListViewModel.settings.sortBy = [.isRefined, .count(order: .descending), .alphabetical(order: .ascending)]
 
 
-    searcherSFFV = FacetSearcher(index: index, query: query, filterState: filterState, facetName: "category", text: "")
+    refinementFacetsViewModel = RefinementFacetsViewModel(selectionMode: .multiple)
+    refinementFacetsViewModel.connect(attribute: Attribute("category"), searcher: searcher, operator: .or)
 
-    textFieldWidget.subscribeToTextChangeHandler { (text) in
-      self.query.page = 0
-      self.searcherSFFV.setQuery(text: text)
-      self.searcherSFFV.search()
-    }
-
-    self.searcherSFFV.onSearchResults.subscribe(with: self) { [weak self] arg in
-
-      let (_, result) = arg
-
-      switch result {
-      case .success(let result):
-        self?.refinementListViewModel.update(with: result)
-        self?.syncRefinementListViewModel.update(with: result)
-
-      case .failure(let error):
-        print(error)
-        break
-      }
-
+    refinementFacetsViewModel.onValuesChanged.subscribe(with: self) { [weak self] (facetValues) in
+      let refinementListPresenter = RefinementListPresenter()
+      self?.sortedFacetValues = refinementListPresenter.processFacetValues(selectedValues: Array(self?.refinementFacetsViewModel.selections ?? Set()), resultValues: facetValues, sortBy: [.isRefined, .count(order: .descending), .alphabetical(order: .ascending)])
       self?.tableView.reloadData()
     }
 
 
-    self.searcher.onSearchResults.subscribePast(with: self) { [weak self] (queryMetada, result) in
-      switch result {
-      case .success(let result):
-        self?.refinementListViewModel.update(with: result)
-        self?.syncRefinementListViewModel.update(with: result)
-      case .failure(let error):
-        print(error)
-        break
-      }
-
-    }
-
-    self.refinementListViewModel.onExecuteNewSearch.subscribe(with: self) { [weak self] in
-      self?.query.page = 0
-      self?.searcher.search()
-    }
-
-    self.refinementListViewModel.onReloadView.subscribe(with: self) { [weak self] in
-        self?.tableView.reloadData()
-    }
-
-    self.refinementListViewModel.onReloadView.subscribe(with: self) {
-      // self?.tableView.reloadData()
-      print("Reloading of the actual second table")
-    }
+//    refinementListViewModel = RefinementListViewModel(attribute: Attribute("category"), filterState: filterState)
+//    syncRefinementListViewModel = RefinementListViewModel(attribute: Attribute("category"), filterState: filterState)
+//    // when "and" + "true", we get a bug in result list, page offset by 1?
+//    // When we use "and" + "true", it's the only time we are actually doing a .search() and not .searchDisjunctiveFaceting()
+//    //refinementListViewModel.settings.operator = .and(selection: .multiple)
+//    refinementListViewModel.settings.operator = .or
+//    refinementListViewModel.settings.sortBy = [.isRefined, .count(order: .descending), .alphabetical(order: .ascending)]
+//
+//    syncRefinementListViewModel.settings.operator = .or
+//    syncRefinementListViewModel.settings.sortBy = [.isRefined, .count(order: .descending), .alphabetical(order: .ascending)]
+//
+//
+//    searcherSFFV = FacetSearcher(index: index, query: query, filterState: filterState, facetName: "category", text: "")
+//
+//    textFieldWidget.subscribeToTextChangeHandler { (text) in
+//      self.query.page = 0
+//      self.searcherSFFV.setQuery(text: text)
+//      self.searcherSFFV.search()
+//    }
+//
+//    self.searcherSFFV.onSearchResults.subscribe(with: self) { [weak self] arg in
+//
+//      let (_, result) = arg
+//
+//      switch result {
+//      case .success(let result):
+//        self?.refinementListViewModel.update(with: result)
+//        self?.syncRefinementListViewModel.update(with: result)
+//
+//      case .failure(let error):
+//        print(error)
+//        break
+//      }
+//
+//      self?.tableView.reloadData()
+//    }
+//
+//
+//    self.searcher.onSearchResults.subscribePast(with: self) { [weak self] (queryMetada, result) in
+//      switch result {
+//      case .success(let result):
+//        self?.refinementListViewModel.update(with: result)
+//        self?.syncRefinementListViewModel.update(with: result)
+//      case .failure(let error):
+//        print(error)
+//        break
+//      }
+//
+//    }
+//
+//    self.refinementListViewModel.onExecuteNewSearch.subscribe(with: self) { [weak self] in
+//      self?.query.page = 0
+//      self?.searcher.search()
+//    }
+//
+//    self.refinementListViewModel.onReloadView.subscribe(with: self) { [weak self] in
+//        self?.tableView.reloadData()
+//    }
+//
+//    self.refinementListViewModel.onReloadView.subscribe(with: self) {
+//      // self?.tableView.reloadData()
+//      print("Reloading of the actual second table")
+//    }
   }
 
   // MARK: - Table View
 
   func numberOfSections(in tableView: UITableView) -> Int {
-    return 2
+    return 1
   }
 
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    if section == 0 {
-      return refinementListViewModel.numberOfFacets()
-    } else {
-      return syncRefinementListViewModel.numberOfFacets()
-    }
+    return sortedFacetValues.count
   }
 
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    if indexPath.section == 0 {
-      refinementListViewModel.didSelectFacet(atIndex: indexPath.row)
-    } else {
-      syncRefinementListViewModel.didSelectFacet(atIndex: indexPath.row)
-    }
+    refinementFacetsViewModel.select(key: sortedFacetValues[indexPath.row].item.value)
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "CellId", for: indexPath)
 
-    let facetValue: FacetValue?
-    if indexPath.section == 0 {
-      facetValue = refinementListViewModel.facet(atIndex: indexPath.row)
-    } else {
-      facetValue = syncRefinementListViewModel.facet(atIndex: indexPath.row)
-    }
-    if let facetValue = facetValue {
-      cell.textLabel?.text = "\(facetValue.value) (\(facetValue.count))"
-      cell.accessoryType = refinementListViewModel.isFacetRefined(atIndex: indexPath.row) ? .checkmark : .none
-    }
+    let selectableRefinement: SelectableRefinement = sortedFacetValues[indexPath.row]
+
+    cell.textLabel?.text = "\(selectableRefinement.item.value) (\(selectableRefinement.item.count))"
+    cell.accessoryType = selectableRefinement.isSelected ? .checkmark : .none
+
     return cell
   }
 
